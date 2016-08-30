ca45315d17681aca2add8d0eef1d0522718056c1
Beans21 <SKardashian19@windwardschool.org>
2016-08-30 08:26:20 -0700

    Version 2 of First Project

--- src/Class_1.java
+++ src/Class_1.java
@@ -3,7 +3,8 @@ public class Class_1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.print("Top of the morning to you.");
+		System.out.println("Top of the morning to you.");
+		System.out.print("Good Morning!");
 	}
 
 }
